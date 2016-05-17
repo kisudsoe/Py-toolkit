bb839230030afbc3da227134eb5cb889372a486f
kisudsoe <kisudsoe@gmail.com>
2016-05-17 21:16:55 +0900

    ss.listIn 함수

---
+++ ss.listIn.py
@@ -0,0 +1,15 @@
+﻿# 2016-01-28 THU
+# Debug listin function
+## get clipboard data
+import win32clipboard # just one-time
+def listIn():
+    win32clipboard.OpenClipboard()
+    clipb = win32clipboard.GetClipboardData()
+    win32clipboard.CloseClipboard()
+    
+    data = clipb.split("\r\n")
+    length = len(data)-1
+    print('list length= %d' % length) # read data as string
+    return(data[0:length])
+# %% Get gene list from clipboard
+data = listIn()

1897f24209a7343396ce28bc9bbb24c3548c86f3
KimSS <kisudsoe@gmail.com>
2017-05-27 18:40:23 +0900

    ss.yeastkoprimer - add blast code
    Add premature local blast codes

--- ss.yeastkoprimer.py
+++ ss.yeastkoprimer.py
@@ -5,7 +5,7 @@ from Bio import SeqIO
 path = "C:/Users/kisud/Desktop/yeast/"
 f_name = ["S288C_YOR040W_GLO4_flanking.fsa",
           "S288C_YLR372W_ELO3_flanking.fsa"]
-for f_line in SeqIO.parse(path+f_name[1],"fasta"):
+for f_line in SeqIO.parse(path+f_name[0],"fasta"):
     seq_name = f_line.id
     sequen = f_line.seq
     seq_len = len(f_line)
@@ -17,7 +17,27 @@ forward = sequen[955:1000]
 rev_tmp = sequen[seq_len-1000:seq_len-955]
 reverse = rev_tmp.reverse_complement()
 print('UP= '+forward+'\n\nrev_tmp= '+rev_tmp+'\nDN= '+reverse+'\n')
-print("%s has read with length %i." %(seq_name,seq_len)+'\n\n'+sequen)
+print("'%s' has read with length %i." %(seq_name,seq_len)+'\n\n'+sequen)
 
 # for primer specificity validation
 # https://www.ncbi.nlm.nih.gov/tools/primer-blast/index.cgi?LINK_LOC=BlastHome
+
+###################################
+# v0.9  - 170527, primer check by Local Blast
+kan_f = "CGTACGCTGCAGGTC"
+kan_r = "ATCGATGAATTCGAG"
+
+from Bio import SeqIO
+path = "C:/Users/kisud/Desktop/yeast/"
+f_name = ["pFA6a-KanMX6.pasta"]
+for f_line in SeqIO.parse(path+f_name[0],"fasta"):
+    seq_vec_name = f_line.id
+    seq_vec = f_line.seq
+    seq_vec_len = len(f_line)
+print("'%s' has read with length %i." %(seq_vec_name,seq_vec_len))
+
+from Bio.Blast.Applications import NcbiblastxCommandline
+#help(NcbiblastxCommandline)
+blastx_cline = NcbiblastxCommandline(query="kanf",db="nr",evalue=0.01, outfmt=5,out=path+"out.xml")
+blastx_cline # 어떻게 보냐 이거
+help(NcbiblastxCommandline)
